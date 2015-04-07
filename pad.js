b3803c1908febebd57adcdfe79310af00f44177f
Edward <edpark13@gmail.com>
2015-04-07 12:53:36 -0700

    create gems add mouse events still need working

--- pad.js
+++ pad.js
@@ -1,18 +1,43 @@
 NUMROWS = 7;
 NUMCOLS = 6;
 GEMR = 50;
-COLORS = ["red", "blue", "green", "yellow", "purple", "pink"]
+COLORS = ["red", "blue", "green", "yellow", "purple", "pink"];
+ACTIVE = false;
+ACTIVEGEM = 'black';
 
 $(function() {
-    for (row=0; row < NUMROWS; row++) {
+    for (col=0; col < NUMCOLS; col++) {
         var column = $("<div class='column'></div>");
-        for (col=0; col < NUMCOLS; col++) {
+        for (row=0; row < NUMROWS; row++) {
             var random = Math.floor(Math.random() * 6);
-            console.log(COLORS[random])
             var gem = $("<div class='gem'> </div>");
             gem.css("background-color", COLORS[random]);
-            gem.appendTo(column)
+            gem.css("top", row*GEMR+"px");
+            gem.css("left", col*GEMR+"px");
+            gem.appendTo(column);
+            // if (ACTIVE) {
+            //     gem.mouseover(move)
+            // }
         }
-        column.appendTo("#board")
+        column.appendTo("#board");
     }
+    $(".gem").mousedown(function() {
+        ACTIVE = true;
+        ACTIVEGEM = $(this);
+        move();
+    })
 });
+
+function move() {
+    $(".gem").mouseover(function () {
+        var tempcolor = $(this).css("background-color");
+        var color = ACTIVEGEM.css('background-color')
+        $(this).css("background-color", color);
+
+    })
+    $(".gem").mouseup(function() {
+        ACTIVE = false;
+        ACTIVEGEM = 'black';
+        console.log(ACTIVE)
+    })
+}
